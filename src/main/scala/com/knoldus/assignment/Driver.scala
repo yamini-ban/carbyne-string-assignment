953911c53122256504c2c087b5634a63b05825a7
yamini-ban <yaminibansal7@gmail.com>
2020-06-05 11:43:57 +0530

    source files added

--- src/main/scala/com/knoldus/assignment/Driver.scala
+++ src/main/scala/com/knoldus/assignment/Driver.scala
@@ -4,8 +4,9 @@ object Driver extends App {
   val str = "Knol is a unit of Knowledge & Dus comes from Druksh which is Sanskrit for a tree," +
     " hence Knoldus is a tree of Knowledge."
   val str1 = "1 hi I'm yamini 2 1."
-  val strOps = new StringOperations(str1)
-  println(strOps.countAlphabetOccurrances())
+  val strOps = new StringOperations
+  println(strOps.countAlphabetOccurrances(str))
+  println(strOps.getStringWithOccurrances(str, 4))
 
 
 }
--- src/main/scala/com/knoldus/assignment/StringOperations.scala
+++ src/main/scala/com/knoldus/assignment/StringOperations.scala
@@ -2,14 +2,19 @@ package com.knoldus.assignment
 
 import scala.collection.mutable
 
-class StringOperations(val str: String) {
+class StringOperations() {
 
-  def countAlphabetOccurrances(): mutable.LinkedHashMap[Char, Int] = {
+  def countAlphabetOccurrances(str: String): mutable.LinkedHashMap[Char, Int] = {
     val map = mutable.LinkedHashMap.empty[Char, Int]
     str
       .toLowerCase
       .foldLeft("")((result, character) => if(character.isLetter) result + character else result)
       .map(ch => map.addOne((ch,str.filter(character => ch == character).length)))
     map
+  }
+
+  def getStringWithOccurrances(str: String, occurrance: Int) = {
+    countAlphabetOccurrances(str).foldLeft("")((result, element) => if(element._2 == occurrance) result + element._1
+    else result)
   }
 }
